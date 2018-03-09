9730e5150fb9ecc58826a43c61e23ec2ed1956c3
Richard Iwasa <riwasa@microsoft.com>
2018-03-09 12:56:00 -0500

    added accelerated networking. fixed bug in simple vm

---
+++ vms/simple-vm-accelerated-networking/simple-vm-accelerated-networking.ps1
@@ -0,0 +1,23 @@
+# *****************************************************************************
+#
+# File:        simple-vm-accelerated-networking.ps1
+#
+# Description: Creates a single Virtual Machine with Accelerated Networking,
+#              if the VM type supports it.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+# *****************************************************************************
+
+$resourceGroupName = Read-Host -Prompt "Enter the name of the Resource Group"
+
+# Create the environment.
+New-AzureRmResourceGroupDeployment -Name simple-vm -ResourceGroupName $resourceGroupName `
+  -TemplateFile "$PSScriptRoot\simple-vm-accelerated-networking-azuredeploy.json" `
+  -TemplateParameterFile "$PSScriptRoot\simple-vm-accelerated-networking-azuredeploy.parameters.json"
