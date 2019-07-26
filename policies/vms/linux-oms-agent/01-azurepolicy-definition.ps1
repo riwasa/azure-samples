2b26c70531d9db3f70f2785e05d6977e1f351393
Richard Iwasa <riwasa@microsoft.com>
2019-07-25 21:02:40 -0400

    Added policy for Linux OMS agent.
    Added policy to deny the OMS agent for Linux VMs.

---
+++ policies/vms/linux-oms-agent/01-azurepolicy-definition.ps1
@@ -0,0 +1,21 @@
+# *****************************************************************************
+#
+# File:        01-azurepolicy-definition.ps1
+#
+# Description: Creates a policy to deny installation of the Linux OMS agent.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+# *****************************************************************************
+
+# Create the Policy definition.
+New-AzPolicyDefinition -Name "deny-linux-oms-agent" `
+  -DisplayName "Deny the Linux OMS agent" `
+  -Description "This policy denies installation of the OMS agent on Linux VMs." `
+  -Policy "$PSScriptRoot\azurepolicy.rules.json"
