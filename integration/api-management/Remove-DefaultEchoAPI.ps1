49f93a7df5d8f4348c0051dc42313b334843b590
Richard Iwasa <riwasa@microsoft.com>
2021-06-09 15:10:31 -0400

    Adds script for removing default APIs
    Adds script to remove default Echo API from APIM services.

---
+++ integration/api-management/Remove-DefaultEchoAPI.ps1
@@ -0,0 +1,31 @@
+# *****************************************************************************
+#
+# File:        Remove-DefaultEchoAPI.ps1
+#
+# Description: Removes the default Echo API from an API Management Service.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+# *****************************************************************************
+
+# Set script variables.
+$resourceGroupName = "rim-man-sec"
+$apimServiceName = "rim-man-sec-apim"
+
+$apimContext = New-AzApiManagementContext -ResourceGroupName $resourceGroupName `
+  -ServiceName $apimServiceName
+
+$apis = Get-AzApiManagementApi -Context $apimContext     
+
+foreach ($api in $apis) {
+  if ($api.Name -eq "Echo API") {
+    Write-Host "Deleting Echo API"
+    Remove-AzApiManagementApi -Context $apimContext -ApiId $api.ApiId
+  }
+}
